df599bd53b8ef19d706c9de024d50b09ff326527
Abhishek Bhadauriya <abhishekbhadauriya7@gmail.com>
2022-02-10 23:02:15 +0530

    "numpy adv 1 completed"

---
+++ numpy/numpy adv 1.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+# 1. Using subscipting and slicing on arrays
+print("array subscripting and slicing...")
+a = np.array([6,7,8])
+print("array subscripting: ",a[-1])
+print("array slicing: ",a[0:2])
+
+print()
+
+print("array subscripting and slicing on 2d array...")
+a = np.array([[6,7,8],[1,2,3],[9,3,2]])
+print("Array: ",a)
+print("picking specific element: \n",a[1,2]) # First row and second column element
+
+print("Array slicing for specific elements: \n",a[0:2,2]) # a[row slicing, column slicing]
+print("Array slicing for specific elements: \n",a[-1,0:2]) # a[row slicing, column slicing]
+print("Array slicing for specific elements: \n",a[:,1:3]) # all rows and 2nd and 3rd column elements
+
+print()
+
+# 2. Iteration on arrays
+print("Iteration on arrays...")
+a = np.array([[6,7,8],[1,2,3],[9,3,2]])
+print("Array: \n",a)
+
+print("Iterating...")
+for row in a:
+    print(row)
+
+# iterating single element by flatening array.
+print("Iterating for single element from array")
+for cell in a.flat: # a.ravel() also works here
+    print(cell)
+
+# 3. Stacking together two arrays
+a = np.arange(6).reshape(3,2)
+b = np.arange(6,12).reshape(3,2)
+print("Array a: \n",a)
+print("Array b: \n",b)
+
+print("Stacking a and b vertically: \n",np.vstack((a,b))) # Note: vstack take tuple as argument
+print("Stacking a and b horizontally: \n",np.hstack((a,b))) # Note: vstack take tuple as argument
+
+print()
+# 4. Spliting bigger array into smaller ones
+print("Spliting bigger array into smaller ones...")
+a = np.arange(30).reshape(2,15)
+print("Array: \n",a)
+
+print("Using hsplit()")
+print("\n",np.hsplit(a,3)) # (axis=0 => column) Vertically splitting into 3 equal sized arrays
+result = np.hsplit(a,3)
+print("\n",result[0])
+print("\n",result[1])
+print("\n",result[2])
+
+
+print("Using vsplit()")
+print("Array: \n",a)
+print("\n",np.vsplit(a,2)) # (axis = 1 => row)Horizontally splitting into 2 equal sized arrays
+result = np.vsplit(a,2)
+print("\n",result[0])
+print("\n",result[1])
